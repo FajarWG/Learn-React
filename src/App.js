5b8da10bfd25141975b583849840c4198940fc0d
FajarWG <fajarwahyugumelar@gmail.com>
2022-07-18 17:50:42 +0700

    Semua Perubahan

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+
 function App() {
 
-  const title = 'Welcome To My App';
-  const age = 19;
-  const link = 'https://www.google.com';
+  const [title, setTitle] = useState("Welcome to My App");
+  const [age, setAge] = useState(20);
+  const [product, setProduct] = useState([
+    {id: 1, title: "Produk 1", price: 200},
+    {id: 2, title: "Produk 2", price: 400},
+    {id: 3, title: "Produk 3", price: 200},
+    {id: 4, title: "Produk 4", price: 700},
+  ]);
+
+  const [text, setText] = useState("");
+
+  const clickMe = () => {
+    setTitle("Title Changed");
+    setAge(40);
+  }
+
+  const deleteData = (productId) =>{
+    const newData = product.filter(product => product.id!== productId);
+    setProduct(newData);
+  }
+
+  const [klik, setKlik] = useState(0);
+  const clickUP = () => {
+    setKlik(klik + 1)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("Total Klik", klik)
+  },[klik]);
 
   return (
     <div>
+      <Header />
       <h2>{ title }</h2>
-      <h2>{ age * 20 }</h2>
-      <a href={ link }>Google</a>
+      <h2>Age : { age }</h2>
+      
+      {klik > 10 ? 
+        <div> 
+          Total klik : {localStorage.getItem("Total Klik")} 
+          <button onClick={() => setKlik(0)}>Reset</button>
+        </div> : 
+        klik
+      }
+
+      <button onClick={ clickUP }>Click Me</button>
+      <br></br>
+      <input onChange={e => setText(e.target.value)}></input>
+
+      <ProductList product={product} deleteData={deleteData}/>
     </div>
   );
 }
